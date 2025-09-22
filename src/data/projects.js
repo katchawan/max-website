984fc1a5aed74d89ccc8531a0e03da4b36c72d93
maxda <maxda@DESKTOP-331S8SC>
2025-09-22 16:13:45 -0500

    New Videos

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,24 +1,24 @@
 //photos you import here, separate by comma
 import {
-  CoxBiz,
-  CoxCon,
-  Dentek,
-  Harley,
-  Stray,
-  Ubrelvy,
-  Xfinity,
+  Image_01,
+  Image_02,
+  Image_03,
+  Image_04,
+  Image_05,
+  Image_06,
+  Image_07,
   insta
 } from '../assets'
 
 //videos imported here, separated by comma
 import {
-  CoxBizVid,
-  CoxConVid,
-  DentekVid,
-  HarleyVid,
-  StrayVid,
-  UbrelvyVid,
-  XfinityVid
+  Video_01,
+  Video_02,
+  Video_03,
+  Video_04,
+  Video_05,
+  Video_06,
+  Video_07
 } from '../assets'
 
 //each project is separated by a bracket and comma
@@ -28,56 +28,56 @@ const projects = [
       password: "",
       hasPassword: false,
       company: "Harley",
-      poster: Harley,
-      source: HarleyVid,
+      poster: Image_01,
+      source: Video_01,
     },
     {
       id: "2",
       password: "",
       hasPassword: false,
       company: "Stray",
-      poster: Stray,
-      source: StrayVid,
+      poster: Image_02,
+      source: Video_02,
     },
     {
       id: "3",
       password: "",
       hasPassword: false,
       company: "Xfinity",
-      poster: Xfinity,
-      source: XfinityVid,
+      poster: Image_03,
+      source: Video_03,
     },
     {
       id: "4",
       password: "",
       hasPassword: false,
       company: "Cox Con",
-      poster: CoxCon,
-      source: CoxConVid,
+      poster: Image_04,
+      source: Video_04,
     },
     {
       id: "5",
       password: "",
       hasPassword: false,
       company: "Ubrelvy",
-      poster: Ubrelvy,
-      source: UbrelvyVid,
+      poster: Image_05,
+      source: Video_05,
     },
     {
       id: "6",
       password: "",
       hasPassword: false,
       company: "Dentek",
-      poster: Dentek,
-      source: DentekVid,
+      poster: Image_06,
+      source: Video_06,
     },
     {
       id: "7",
       password: "",
       hasPassword: false,
       company: "CoxBiz",
-      poster: CoxBiz,
-      source: CoxBizVid,
+      poster: Image_07,
+      source: Video_07,
     }
 ];
 
